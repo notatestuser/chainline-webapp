3bd4bc6e546a2f5a1fdf187d0e36226b4e662ad0
Luke Plaster <me@lukep.org>
2017-10-25 03:23:46 +0800

    Showing the wallet balance's USD value

--- src/js/components/WalletWidget.js
+++ src/js/components/WalletWidget.js
@@ -1,34 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
+import numeral from 'numeral';
 
 import { Menu } from 'grommet';
 import { Money, Alert } from 'grommet-icons';
 
-const WalletWidget = ({ accountWif, onOpenWalletClick }) => {
-  if (accountWif) {
-    // logged in
+import { getBalance, getPrice } from 'chainline-js';
+
+class WalletWidget extends Component {
+  state = {
+    balance: null,
+    gasPriceUSD: null,
+  }
+
+  componentDidMount() {
+    getBalance('TestNet', 'AenDCN3Xw3zXC5S5BNbEgT4UmDh6WPg8a1')
+      .then((balance) => {
+        this.setState({ balance: balance.GAS.balance });
+      });
+    getPrice('GAS')
+      .then((gasPriceUSD) => {
+        this.setState({ gasPriceUSD });
+      });
+  }
+
+  render() {
+    const { accountWif, onOpenWalletClick } = this.props;
+    const { balance, gasPriceUSD } = this.state;
+
+    if (accountWif) {
+      // logged in
+      return (<Menu
+        background='neutral-5'
+        full='grow'
+        label={<strong>
+          Wallet Balance: {typeof balance === 'number' ?
+            numeral(balance).format('0,0.00') : '?'} GAS
+          {typeof gasPriceUSD === 'number' && typeof balance === 'number' ?
+            ` ($${numeral(gasPriceUSD * balance).format('0,0.00')})` : ''}
+        </strong>}
+        icon={<Money />}
+        dropAlign={{ right: 'right', top: 'top' }}
+        items={[
+          { label: 'Reserved: 0 GAS' },
+          { label: 'Send Money' },
+          { label: 'Receive Money' }]}
+      />);
+    }
+    // not logged in
     return (<Menu
       background='neutral-5'
       full='grow'
-      label={<strong>Wallet Balance: 0 GAS</strong>}
-      icon={<Money />}
+      label={<strong>You are not logged in!</strong>}
+      icon={<Alert />}
       dropAlign={{ right: 'right', top: 'top' }}
       items={[
-        { label: 'Reserved: 0 GAS' },
-        { label: 'Send Money' },
-        { label: 'Receive Money' }]}
+        { label: 'Create a new wallet' },
+        { label: 'Load a wallet', onClick: () => onOpenWalletClick() }]}
     />);
   }
-  // not logged in
-  return (<Menu
-    background='neutral-5'
-    full='grow'
-    label={<strong>You are not logged in!</strong>}
-    icon={<Alert />}
-    dropAlign={{ right: 'right', top: 'top' }}
-    items={[
-      { label: 'Create a new wallet' },
-      { label: 'Load a wallet', onClick: () => onOpenWalletClick() }]}
-  />);
-};
+}
 
 export default WalletWidget;
--- src/js/screens/Home.js
+++ src/js/screens/Home.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Heading } from 'grommet';
 
-import { getBalance } from 'chainline-js';
-
 import { Box, WidthCappedContainer } from '../components';
 
 export default class Home extends Component {
-  state = {}
+  state = {
+    balance: 'unknown',
+  }
 
   componentDidMount() {
-    getBalance('TestNet', 'AenDCN3Xw3zXC5S5BNbEgT4UmDh6WPg8a1')
-      .then((balance) => {
-        this.setState({ balance: balance.GAS.balance });
-      });
+    // getBalance('TestNet', 'AenDCN3Xw3zXC5S5BNbEgT4UmDh6WPg8a1')
+    //   .then((balance) => {
+    //     this.setState({ balance: balance.GAS.balance });
+    //   });
   }
 
   render() {
