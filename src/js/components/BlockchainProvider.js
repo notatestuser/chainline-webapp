4155da1b4860bc57a6e6e2224e6fb430ccb0be81
Luke Plaster <me@lukep.org>
2017-11-17 20:00:10 +0800

    Using the CL balance api

--- src/js/components/BlockchainProvider.js
+++ src/js/components/BlockchainProvider.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';  // eslint-disable-line
 import PropTypes from 'prop-types';
 import numeral from 'numeral';
 import is from 'is_js';
+import unfetch from 'isomorphic-unfetch';
+
 import {
   getAccountFromWIFKey,
-  getBalance,
   getPrice,
   getWalletState,
 } from 'chainline-js';
 
+const CHAINLINE_BALANCE_API = 'https://chainline.co/api/balance/';
 const REFRESH_INTERVAL_MS = 15000;
 const NUMBER_FORMAT = '0,0.000';
 
@@ -102,20 +104,20 @@ class BlockchainProvider extends Component {
     const refresh = async () => {
       console.debug('Refreshing wallet state…');
       // in parallel, fail-safe
-      getBalance(net, address, true).then((response) => {
+      const res = await unfetch(`${CHAINLINE_BALANCE_API}${address}`);
+      if (res.ok) {
         const { reserved } = this.state;
-        const newBalance = response.GAS.balance;
+        const { balance: newBalance } = await res.json();
         // avoid unnecessary state updates
         if (newBalance === this.state.originalBalance) return;
         this.setState({
           originalBalance: newBalance,
           balance: reserved ? newBalance - reserved : newBalance,
         });
-      }).catch((err) => {
-        const { message } = err;
+      } else {
+        const message = res.statusText;
         console.error(`Could not get wallet balance! ${message}`);
-        console.error('getBalance error', err);
-      });
+      }
       getWalletState(net, wif, programHash).then((response) => {
         const { originalBalance } = this.state;
         const { reservedBalance, reputation, stateLookupKey } = response;
